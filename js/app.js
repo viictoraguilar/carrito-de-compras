da169465353ebda2efce37b8ebb40fdc80528134
Víctor Hugo <victorhugo.webdeveloper@gmail.com>
2021-01-29 14:07:49 -0600

    Leyendo datos del curso seleccionando

--- js/app.js
+++ js/app.js
@@ -15,8 +15,25 @@ function cargarEventListeners() {
 function agregarCurso(e) {
   e.preventDefault()
   if(e.target.classList.contains('agregar-carrito')) {
-    console.log(e.target)
+    const cursoSeleccionado = e.target.parentElement.parentElement
+
+    leerDatosCurso(cursoSeleccionado)
   }
 }
 
+//Lee el contenido del html al que se le dio clicj y extrae la información del curso
+function leerDatosCurso(curso) {
+  console.log(curso)
+
+  //Crear un objeto con el contenido del curso actual
+  const inforCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1
+  }
+
+  console.log(inforCurso)
+}
 
